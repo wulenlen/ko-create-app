0d7bddb1e270cf465de2d71cc46a2a418a2b0e67
yangyi <yhxyangyi@163.com>
2020-03-02 11:02:00 +0800

    版本-update

---
+++ packages/kov-base/es/baseLine/index.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+export function BaseLineHorizontal(props) {
+  const { width, config = {}, scale, max } = props;
+  const { baseValue, baseType, show, baseLine = {}, text = {} } = config;
+  const { color, lineType } = baseLine;
+  if (!show) return null;
+  const y = baseType === "max" ? scale(max) : scale(+baseValue);
+  if (+baseValue > max) {
+    return null;
+  }
+  return (
+    <g className="__easyv-baseline">
+      <line
+        x2={width}
+        y1={y}
+        y2={y}
+        stroke={baseLine.color}
+        strokeDasharray={baseLine.lineType === "dash" ? "3 3" : null}
+      />
+      {text.show && (
+        <text
+          x={!isNaN(text.x) ? text.x : width + 3}
+          y={y}
+          fill={text.color}
+          fontSize={text.fontSize}
+          fontFamily={text.fontFamily}
+          dy="0.3em"
+        >
+          {text.content}
+        </text>
+      )}
+    </g>
+  );
+}
+
+export function BaseLineVertical(props) {
+  const { height, config = {}, scale, max } = props;
+  const { baseValue, baseType, show, baseLine = {}, text = {} } = config;
+  if (!show) return null;
+  const x = baseType === "max" ? scale(max) : scale(+baseValue);
+  return (
+    <g className="__easyv-baseline">
+      <line
+        x1={x}
+        x2={x}
+        y1={0}
+        y2={height}
+        stroke={baseLine.color}
+        strokeDasharray={baseLine.lineType === "dash" ? "3 3" : null}
+      />
+      {text.show && (
+        <text
+          x={x}
+          y={-10}
+          fill={text.color}
+          fontSize={text.fontSize}
+          fontFamily={text.fontFamily}
+          textAnchor="middle"
+        >
+          {text.content}
+        </text>
+      )}
+    </g>
+  );
+}
---
+++ packages/kov-base/es/echart/index.js
@@ -0,0 +1,9 @@
+import echarts from 'echarts/lib/echarts'; 
+import 'echarts/lib/component/grid';
+import 'echarts/lib/component/markLine'
+import 'echarts/lib/component/dataZoom'
+import 'echarts/lib/component/tooltip'
+import 'echarts/lib/component/title'
+import 'echarts/lib/component/legend'
+import 'zrender/lib/svg/svg';
+export default echarts;
