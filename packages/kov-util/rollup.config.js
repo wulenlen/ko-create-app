4dd0db9a619b4256abf56c471be973eb1c1e2b1f
yangyi <yhxyangyi@163.com>
2020-03-04 17:36:41 +0800

    版本-update

--- packages/kov-util/rollup.config.js
+++ packages/kov-util/rollup.config.js
@@ -12,7 +12,7 @@ export default {
   input: path.join(__dirname,'src/index.js'),
   output: {
     name:'util',  //当format为iife和umd时必须提供，将作为全局变量挂在window(浏览器环境)下：window.A=...
-    file: path.join(__dirname,process.env.NODE_ENV === 'production' ? 'lib/ko-util.min.js':'lib/ko-util.js'),
+    file: path.join(__dirname,process.env.NODE_ENV === 'production' ? 'lib/kov-util.min.js':'lib/kov-util.js'),
     format: 'umd' //  五种输出格式：amd /  es6 / iife / umd / cjs
   },
   plugins: [
